b1ff5a05ef339c801d86d974e0e8be09543d51ed
Quentin <bonaventurequentin@gmail.com>
2022-07-05 13:51:43 +0200

    Creatie page login

--- Technifutur-Ios-FilRouge/Technifutur-Ios-FilRouge/ViewController.swift
+++ Technifutur-Ios-FilRouge/Technifutur-Ios-FilRouge/ViewController.swift
@@ -8,11 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            view.endEditing(true)
+        }
 
 
 }
